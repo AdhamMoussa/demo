c9fd9b8128d74588966837f6faa27ce83b9b3ba8
Adham Moussa <adam.moussa62017@gmail.com>
2018-10-19 18:13:47 +0200

    new tender

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,24 @@
-import $ from 'jquery';
+import $ from "jquery";
 
-const el = $(document);
+$(function() {
+  // form file-input
+  const fileInput = $("#pdf-input");
+  const fileInputLabel = $("#pdf-input ~ label");
+  const fileInputPlaceholder = $("#pdf-input-placeholder");
+
+
+  // form file-input functionality
+  if (fileInput.length > 0) {
+    fileInput.change(e => {
+      if (fileInput[0].files.length > 0) {
+        const fileName = fileInput[0].files[0].name;
+        fileInputPlaceholder.html(fileName);
+      } else {
+        fileInputPlaceholder.html("PDF *");
+      }
+    });
+    fileInputPlaceholder.click(() => {
+      fileInputLabel.click();
+    });
+  }
+});
